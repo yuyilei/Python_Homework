7f453aedffb0c78cfc0da2359fa93f95cd04a0cd
yuyilei <yyl1352091742@gmail.com>
2017-04-20 22:56:00 +0800

    生成器,杨辉三角形

--- 4-19-1.py
+++ 4-19-1.py
@@ -2,7 +2,8 @@
 #生成器，输出杨辉三角形
 def triangle(n) :
     b = [1]
-    t = 0
+    yield(b)
+    t = 1
     while t < n  :
         b = [1] + [ b[i] + b[i+1] for i in range(len(b)-1)] + [1]
         t += 1
