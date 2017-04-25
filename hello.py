5e60c425deeabd599269a602fb5055bd004cb6bc
yuyilei <yyl1352091742@gmail.com>
2017-04-25 22:15:46 +0800

    hello 模块

---
+++ hello.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+#coding: utf-8
+
+import sys
+def test() :
+    args = sys.argv
+    if   len(args) == 1 :
+        print('hello,world!')
+    elif len(args) == 2 :
+        print('hello,%s' % args[1])
+    else :
+        print('Too many arguments!')
+
+if __name__ == '__main__' :
+    test()
